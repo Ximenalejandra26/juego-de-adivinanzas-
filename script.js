9b59d3a4b057b4cbeacf986d7ca4903285991b4e
Ximena <Ximena.aleja021@outlook.com>
2024-10-28 22:10:14 -0500

    por ultimo solicitamos reultado con while

--- script.js
+++ script.js
@@ -4,4 +4,18 @@ let num = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo; // Generar
 let intentos = 0;
 let usuario;
 
+do {
+    usuario = parseInt(prompt(`Ingresa un número entre ${minimo} y ${maximo}:`));
+    intentos++;
+
+    if (num > usuario) {
+        document.write("El número que buscas es mayor.<br>");
+    } else if (num < usuario) {
+        document.write("El número que buscas es menor.<br>");
+    } else {
+        document.write(`¡Felicidades! Adivinaste el número ${num} en ${intentos} intentos.`);
+    }
+} while (num !== usuario);
+
+
    
